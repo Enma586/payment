e6edcac3abd31a6dfb5de822678e09783d83c07c
Enma586 <enmaflore7421@gmail.com>
2026-04-18 15:32:43 -0600

    update config.json and add transactions model and migration

--- migrations/20260418213101-create-transactions.js
+++ migrations/20260418213101-create-transactions.js
@@ -2,21 +2,39 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      externalId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      amount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM('RECEIVED', 'PROCESSING', 'COMPLETED', 'FAILED'),
+        defaultValue: 'RECEIVED'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
   }
 };
